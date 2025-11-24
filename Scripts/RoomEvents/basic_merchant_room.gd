231bc0b201c71136655c96ceb305fb8d87191066
cartoonsandcoffee <justmoran@gmail.com>
2025-11-24 13:29:45 -0500

    Add files via upload

--- Scripts/RoomEvents/basic_merchant_room.gd
+++ Scripts/RoomEvents/basic_merchant_room.gd
@@ -1,18 +1,21 @@
 class_name BasicMerchantRoomEvent
 extends RoomEvent
 
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
-@onready var button: Button = $Merchant/Button
+@onready var button: Button = $picEvent/Button
 @onready var item_store: ItemStore = $item_merchant_store
 @onready var lbl_action: Label = $lblAction
 
+@onready var anim_label: AnimationPlayer = $animLabel
+@onready var anim_event: AnimationPlayer = $animEvent
 
 func _on_button_mouse_exited() -> void:
-	lbl_action.visible = false
+	anim_label.play("hide_label")
+	await anim_label.animation_finished
 
 
 func _on_button_mouse_entered() -> void:
-	lbl_action.visible = true
+	anim_label.play("show_label")
+	await anim_label.animation_finished
 
 
 func _ready() -> void:
@@ -33,18 +36,18 @@ func _run_room_event():
 	show_merchant()
 
 func show_merchant():
-	anim_player.play("show_merchant")
-	await anim_player.animation_finished
+	anim_event.play("show_event")
+	await anim_event.animation_finished
 
 func play_merchant_idle():
-	anim_player.play("merchant_idle")
+	anim_event.play("event_idle")
 
 func _on_button_pressed() -> void:
 	item_store.show_store()
 
 func close_up_shop():
-	anim_player.play("hide_merchant")
-	await anim_player.animation_finished
+	anim_event.play("hide_event")
+	await anim_event.animation_finished
 	complete_event()
 
 func _on_item_selected(item: Item):
